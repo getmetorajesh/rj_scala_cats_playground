3a52ea4c38e8c72048f4be90a551e8af54938400
rj <rajesh@mylocalbroker.com.au>
2016-06-08 00:00:17 +1000

    cats show and partial order

--- .worksheet/src/com.example.day1.scala
+++ .worksheet/src/com.example.day1.scala
@@ -4,12 +4,14 @@ import cats.std.all._
 import cats.syntax.eq._
 import cats.syntax.order._
 import cats.syntax.partialOrder._
+
 /*
 Eq
 Ord - is for types that have an ordering. ord covers all teh standard comparing fn <.<,>= and <=
-
+PartialOrder - enables tryCompare syntax which returns Option[Int]
+Show - similar to toString on Any except with Any you lose typesafety
 */
-object day1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(506); 
+object day1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(643); 
 
   /* Eq enables === and =!= syntax by declaring eqv method.
 	Instead of the standard ==, Eq enables === and =!= syntax. main diff is === would fail compilation
@@ -29,8 +31,21 @@ object day1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; 
  */
   1 > 2.0;System.out.println("""res2: Boolean(false) = """ + $show(res$2));$skip(18); val res$3 = 
   1.0 compare 2.0;System.out.println("""res3: Int = """ + $show(res$3));$skip(14); val res$4 = 
-  1.0 max 2.0;System.out.println("""res4: Double = """ + $show(res$4))}
+  1.0 max 2.0
 
   // PartialOrder enables tryCompare syntax which returns Option[Int]
+  import cats.syntax.partialOrder._;System.out.println("""res4: Double = """ + $show(res$4));$skip(124); val res$5 = 
+  1 tryCompare 2;System.out.println("""res5: Option[Int] = """ + $show(res$5));$skip(28); val res$6 = 
+  1.0 tryCompare Double.NaN;System.out.println("""res6: Option[Int] = """ + $show(res$6));$skip(16); 
 
+  day11.func()}
+}
+
+object day11 {
+  def func() {
+    import cats.syntax.show._
+    println((new {}).toString())
+     println((new {}).show)
+    //(new {}).show()
+  }
 }
--- src/main/scala/com/example/day1.sc
+++ src/main/scala/com/example/day1.sc
@@ -4,10 +4,12 @@ import cats.std.all._
 import cats.syntax.eq._
 import cats.syntax.order._
 import cats.syntax.partialOrder._
+
 /*
 Eq
 Ord - is for types that have an ordering. ord covers all teh standard comparing fn <.<,>= and <=
-
+PartialOrder - enables tryCompare syntax which returns Option[Int]
+Show - similar to toString on Any except with Any you lose typesafety
 */
 object day1 {
 
@@ -16,21 +18,34 @@ object day1 {
 	if tried to compare Int and String
 */
 
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  println("Welcome to the Scala worksheet")
 
   //1 === "foo"
 
   // returns true if x and y are equivalent
-  eq(1, "foo")                                    //> res0: Boolean = false
-  ne(1, 1)                                        //> res1: Boolean = true
+  eq(1, "foo")
+  ne(1, 1)
 
   /* Order enables compare syntac which returns Int: neg, zero or post.
  	
  */
-  1 > 2.0                                         //> res2: Boolean(false) = false
-  1.0 compare 2.0                                 //> res3: Int = -1
-  1.0 max 2.0                                     //> res4: Double = 2.0
+  1 > 2.0
+  1.0 compare 2.0
+  1.0 max 2.0
 
   // PartialOrder enables tryCompare syntax which returns Option[Int]
+  import cats.syntax.partialOrder._
+  1 tryCompare 2
+  1.0 tryCompare Double.NaN
 
+  day11.func()
+}
+
+object day11 {
+  def func() {
+    import cats.syntax.show._
+    println((new {}).toString())
+     println((new {}).show)
+    //(new {}).show()
+  }
 }
